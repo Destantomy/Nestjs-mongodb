e67b566597fa8d38e75c0a4839d7f3f8d0d6f940
Destanto M. Y. <destantomy8@gmail.com>
2026-01-15 13:05:47 +0700

    03-registeringSchema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/')],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/'), UserModule],
   controllers: [],
   providers: [],
 })
